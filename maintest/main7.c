4c68dc72435c65d31372466ec98ae9a4b957c693
“”tom8760925 <tom8760925@gmail.com>
2024-03-14 17:28:23 +0800

    up

---
+++ maintest/main7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    printf("\\n:A\n");//換行
+    printf("\\t:\tA\n");//tab
+    printf("\\b:\bA\n");//backspace
+    printf("\\r:\rABC\n");
+    printf("\\f:\fA\n");//跳到下一行
+    printf("\\':\'A\n");
+    printf("\\\":\"A\n");
+    printf("\\:\\A\n");
+    return 0;
+}
