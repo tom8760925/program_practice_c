de79b927aa93db12bedde158204b69ffe14d6c77
“”tom8760925 <tom8760925@gmail.com>
2024-03-16 21:30:43 +0800

    20240316

--- practice/student.c
+++ practice/student.c
@@ -2,7 +2,7 @@
 #ifndef NMAX //設定最大值
 #define NMAX 3
 #endif
-struct student
+struct student 
 {
     char name[50]; //姓名
     int no,phore; //學號、電話
@@ -10,10 +10,10 @@ struct student
 int main(){
     char *n[NMAX] = {"姓名","學號","電話"}; //設定初值
     struct student st; //呼叫student
-    for(int i=0;i<NMAX;i++){
-        printf("請輸入%s:",n[i]);
-        scanf("%s",st.name);
-        printf("%s:%s\n",n[i],st.name);
+    for(int i=0;i<NMAX;i++){//跑設定的次數
+        printf("請輸入%s:",n[i]);//顯示需要輸入的值
+        scanf("%s",st.name);//輸入的值
+        printf("%s:%s\n",n[i],st.name);//顯示輸入的值
     }
     return 0;
 }
